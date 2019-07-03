784c726ddb560149b5f2911324be097af0949a88
Stefan Kühnlein <stefan.kuehnlein@opitz-consulting.com>
2019-07-03 08:36:03 +0200

    timeout

--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -21,7 +21,7 @@ oc create configmap tasks-blue-config --from-literal="application-users.properti
 oc set volume dc/tasks-blue --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-blue --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
 oc set probe dc/tasks-blue --readiness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
 oc set env dc/tasks-blue VERSION='0.0 (tsks-blue)' -n ${GUID}-tasks-prod
 
@@ -34,7 +34,7 @@ oc create configmap tasks-green-config --from-literal="application-users.propert
 oc set volume dc/tasks-green --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-green --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
 oc set probe dc/tasks-green --readiness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
 oc set env dc/tasks-green VERSION='0.0 (tsks-green)' -n ${GUID}-tasks-prod
 
